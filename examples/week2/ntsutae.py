4cd08088f40ba899c09fac8c8807f0a5851eb5b1
colormotor <drand48@gmail.com>
2024-10-08 21:27:14 +0100

    ..fix

--- examples/week2/ntsutae.py
+++ examples/week2/ntsutae.py
@@ -9,23 +9,20 @@ See: https://twitter.com/ntsutae/status/1268820823952916486?s=20&t=F8ydakRFTRmTl
 from py5canvas import *
 
 def setup():
-    create_canvas(300, 300)
+    create_canvas(600, 600)
     background(0)
-    stroke_weight(2)
 
 def draw():
     t = frame_count
     copy(0, 0, width, height, 0, 1, width, height)  # scroll down
     stroke(255)
     for x in range(width, -1, -1):
-        #v = int(abs(t+((x-t)**(x+t)+3)**9))%997 > 97;
-        #v = abs(t + ((x - t) ** (x + t) + 3) ** 9) % 997 > 97  # original version
-        v = ((x + t) ^ (x - t)) % 9  # simplified version
+        v = int(abs(t+((x-t)^(x+t)+3)**9))%997 > 97;
+        #v = ((x + t) ^ (x - t)) % 9  # simplified version
         if not v:  # when v is 0, stroke white
             stroke(255)
         else:
             stroke(0)
-
         point(x, 0)
 
 run()
